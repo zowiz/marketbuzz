ed96654896ef75b0301498185bc4f323e875ba0c
zowiz <prodemon@gmail.com>
2026-09-16 11:27:18 +0530

    Fresh

--- api/market.js
+++
@@ -1,8 +0,0 @@
-import { list } from '@vercel/blob';
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin','*');
-  const { blobs } = await list({ prefix: 'market.json' });
-  if(!blobs.length) return res.status(200).json({ headline: "Market loading..." });
-  const data = await fetch(blobs[0].url).then(r=>r.json());
-  res.status(200).json(data);
-}
