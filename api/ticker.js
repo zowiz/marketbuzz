d2369307e440e78d7d3967b99155fc100b00b205
zowiz <prodemon@gmail.com>
2026-09-18 14:07:45 +0530

    update market data

--- api/ticker.js
+++ api/ticker.js
@@ -8,8 +8,14 @@ export default async function handler(req, res) {
     const sensex = sensexData.chart.result[0];
     res.setHeader('Cache-Control', 'no-store');
     res.status(200).json({
-      nifty: { price: nifty.meta.regularMarketPrice, change: nifty.meta.regularMarketPrice - nifty.meta.previousClose, changePercent: ((nifty.meta.regularMarketPrice - nifty.meta.previousClose) / nifty.meta.previousClose * 100) },
-      sensex: { price: sensex.meta.regularMarketPrice, change: sensex.meta.regularMarketPrice - sensex.meta.previousClose, changePercent: ((sensex.meta.regularMarketPrice - sensex.meta.previousClose) / sensex.meta.previousClose * 100) },
+      nifty: {
+        price: nifty.meta.regularMarketPrice,
+        change: nifty.meta.regularMarketPrice - nifty.meta.previousClose
+      },
+      sensex: {
+        price: sensex.meta.regularMarketPrice,
+        change: sensex.meta.regularMarketPrice - sensex.meta.previousClose
+      },
       time: new Date().toISOString()
     });
   } catch (e) {
